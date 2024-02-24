c143379560cb3e56c374edd1e001d664aed3748f
av6920337 <av6920337@gmail.com>
2024-02-24 05:18:34 +0100

    first commit

---
+++ assets/scripts/index.js
@@ -0,0 +1,25 @@
+const alphabet=' абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
+console.log(alphabet);
+
+// const randomIndex1=Math.random()*alphabet;
+// const randomIndex2=Math.random()*alphabet;
+// const randomIndex3=Math.random()*alphabet;
+// const randomIndex4=Math.random()*alphabet;
+
+// const randomWord=alphabet[randomIndex1]+alphabet[randomIndex2]+alphabet[randomIndex3]+alphabet[randomIndex4];
+
+// console.log(randomWord);
+// 1. Создаем переменную с алфавитом
+// var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+// // 2. Создаем четыре случайных индекса
+// var randomIndex1 = Math.floor(Math.random() * alphabet.length);
+// var randomIndex2 = Math.floor(Math.random() * alphabet.length);
+// var randomIndex3 = Math.floor(Math.random() * alphabet.length);
+// var randomIndex4 = Math.floor(Math.random() * alphabet.length);
+
+// // 3. Извлекаем символы из алфавита по полученным индексам и объединяем их в строку
+// var randomWord = alphabet[randomIndex1] + alphabet[randomIndex2] + alphabet[randomIndex3] + alphabet[randomIndex4];
+
+// // 4. Выводим полученное случайное слово на экран
+// console.log(randomWord);
