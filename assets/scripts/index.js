580184fc5fb604ee4b1d1d4016d0bd5aa6775478
av6920337 <av6920337@gmail.com>
2024-02-24 13:13:03 +0100

    second commit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,25 +1,29 @@
-const alphabet=' абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
-console.log(alphabet);
-
-// const randomIndex1=Math.random()*alphabet;
-// const randomIndex2=Math.random()*alphabet;
-// const randomIndex3=Math.random()*alphabet;
-// const randomIndex4=Math.random()*alphabet;
-
-// const randomWord=alphabet[randomIndex1]+alphabet[randomIndex2]+alphabet[randomIndex3]+alphabet[randomIndex4];
-
-// console.log(randomWord);
 // 1. Создаем переменную с алфавитом
-// var alphabet = "abcdefghijklmnopqrstuvwxyz";
+// const alphabet=['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я'];
 
-// // 2. Создаем четыре случайных индекса
-// var randomIndex1 = Math.floor(Math.random() * alphabet.length);
-// var randomIndex2 = Math.floor(Math.random() * alphabet.length);
-// var randomIndex3 = Math.floor(Math.random() * alphabet.length);
-// var randomIndex4 = Math.floor(Math.random() * alphabet.length);
+// 2. Создаем четыре случайных индекса
+// const randomIndex1=Math.floor(Math.random()*alphabet.length);
+// const randomIndex2=Math.floor(Math.random()*alphabet.length);
+// const randomIndex3=Math.floor(Math.random()*alphabet.length);
+// const randomIndex4=Math.floor(Math.random()*alphabet.length);
 
-// // 3. Извлекаем символы из алфавита по полученным индексам и объединяем их в строку
-// var randomWord = alphabet[randomIndex1] + alphabet[randomIndex2] + alphabet[randomIndex3] + alphabet[randomIndex4];
+// 3. Извлекаем символы из алфавита по полученным индексам и объединяем их в строку
+// const randomWord=alphabet[randomIndex1]+alphabet[randomIndex2]+alphabet[randomIndex3]+alphabet[randomIndex4];
 
 // // 4. Выводим полученное случайное слово на экран
 // console.log(randomWord);
+
+// 1. Создаем переменную с алфавитом
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+// 2. Создаем четыре случайных индекса
+var randomIndex1 = Math.floor(Math.random() * alphabet.length);
+var randomIndex2 = Math.floor(Math.random() * alphabet.length);
+var randomIndex3 = Math.floor(Math.random() * alphabet.length);
+var randomIndex4 = Math.floor(Math.random() * alphabet.length);
+
+// // 3. Извлекаем символы из алфавита по полученным индексам и объединяем их в строку
+var randomWord = alphabet[randomIndex1] + alphabet[randomIndex2] + alphabet[randomIndex3] + alphabet[randomIndex4];
+
+// // 4. Выводим полученное случайное слово на экран
+console.log(randomWord);
